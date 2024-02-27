e847b1e0ea7e9e34517cfd81a3525bb681d43891
Razzi Abuissa <razzi@abuissa.net>
2024-02-27 11:23:01 -0600

    Remove clustering and hardcode port to 3000

--- index.js
+++ index.js
@@ -7,20 +7,6 @@ const { join } = require('node:path');
 const { Server } = require('socket.io');
 const sqlite3 = require('sqlite3');
 const { open } = require('sqlite');
-const { availableParallelism } = require('node:os');
-const cluster = require('node:cluster');
-const { createAdapter, setupPrimary } = require('@socket.io/cluster-adapter');
-
-if (cluster.isPrimary) {
-  const numCPUs = availableParallelism();
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork({
-      PORT: 3000 + i
-    });
-  }
-
-  return setupPrimary();
-}
 
 async function main() {
   const db = await open({
@@ -40,7 +26,6 @@ async function main() {
   const server = createServer(app);
   const io = new Server(server, {
     connectionStateRecovery: {},
-    adapter: createAdapter()
   });
 
   app.get('/', (req, res) => {
@@ -82,14 +67,11 @@ async function main() {
     }
   });
 
-  const port = process.env.PORT;
+  const port = 3000;
 
   server.listen(port, () => {
     console.log(`server running at http://localhost:${port}`);
   });
-  // server.listen(3000, () => {
-  //   console.log('server running at http://localhost:3000');
-  // });
 }
 
 main();
